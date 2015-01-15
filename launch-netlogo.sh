66650c0ac4594d02d8031f12ace46a7288178c29
Bryan Head <bryan.head@gmail.com>
2015-01-15 10:07:53 -0600

    Make work with any NetLogo version

--- launch-netlogo.sh
+++ launch-netlogo.sh
@@ -1,2 +1,4 @@
-open -n '../../../NetLogo 5.1.0.app'
-osascript -e "tell application \"NetLogo 5.1.0\" to activate"
+cd ../../../
+app=`ls -1 | grep -e 'NetLogo [0-9\.]*\.app'`
+open -n "$app"
+osascript -e "tell application \"${app%.*}\" to activate"
--- makefile
+++ makefile
@@ -1,6 +1,9 @@
 all:
 	platypus -a 'NetLogo Launcher' -i NetLogo.icns -o 'None' -R -y launch-netlogo.sh
 
+debug:
+	platypus -a 'NetLogo Launcher' -i NetLogo.icns -y launch-netlogo.sh
+
 clean:
 	rm -rf 'NetLogo Launcher.app'
 
