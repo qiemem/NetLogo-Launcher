b52897e100d791b983e7bbd05f5fbeac66787f46
Bryan Head <bryan.head@gmail.com>
2015-01-17 13:57:30 -0600

    Fix app finding, add open with support, add error dialog

--- launch-netlogo.sh
+++ launch-netlogo.sh
@@ -1,13 +1,20 @@
 cd ../../../
-app=`ls -1 | grep -e 'NetLogo [0-9\.]*\.app'`
+app=`ls -1 | grep -e 'NetLogo [0-9.]*\.app'`
+ls
+
+if [ -z "${app// }" ]; then
+    path=`pwd`
+    osascript -e "display dialog \"I couldn't find a NetLogo app in:\n\n${path}\n\nMake sure NetLogo Launcher is in the same folder as the NetLogo app.\" buttons {\"Quit\"}"
+    exit 1
+fi
 
 if [ "$#" -ge 1 ]; then
-	for var in "$@"
-	do
-		open -n "$app" "$var"
-	done
+    for var in "$@"
+    do
+	open -n "$app" --args "$var"
+    done
 else
-	open -n "$app"
+    open -n "$app"
 fi
 
 osascript -e "tell application \"${app%.*}\" to activate"
--- makefile
+++ makefile
@@ -1,8 +1,8 @@
 all:
-	platypus -a 'NetLogo Launcher' -i NetLogo.icns -o 'None' -R -D -y launch-netlogo.sh
+	platypus -a 'NetLogo Launcher' -X "nlogo" -i NetLogo.icns -Q Model.icns -o 'None' -R -D -y launch-netlogo.sh
 
 debug:
-	platypus -a 'NetLogo Launcher' -i NetLogo.icns -D -y launch-netlogo.sh
+	platypus -a 'NetLogo Launcher' -X "nlogo" -i NetLogo.icns -Q Model.icns -D -y launch-netlogo.sh
 
 clean:
 	rm -rf 'NetLogo Launcher.app'
